2f24009aae7499bea6bea499d5089ef16bed565b
hardikmodi1 <hmodi2457@gmail.com>
2024-09-25 11:02:41 +0530

    fix: add doc for clearAllMocks

--- app/exercises/mockClear.spec.tsx
+++ app/exercises/mockClear.spec.tsx
@@ -1,7 +1,9 @@
 /**
  * 😮‍💨 Finally, we have a simple jest method to understand.
  * mockClear
+ * clearAllMocks
  * This function doesn't do any extra thing one and simple task of just clearing out mock related things like calls, etc
+ * clearAllMocks is like calling mockClear on all mocked functions. But jest does it automatically. We just have to call it.
  */
 import * as utils from "../utils/getSystemTimezone";
 
